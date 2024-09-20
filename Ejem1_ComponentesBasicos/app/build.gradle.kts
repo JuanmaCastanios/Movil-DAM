6e29fcd70ec56f2adecd5efa876d9536a470f902
JuanmaCastanios <juanjoseblancodiaz1504@gmail.com>
2024-09-20 10:05:03 +0200

    Prueba con botones y campos editables

--- Ejem1_ComponentesBasicos/app/build.gradle.kts
+++ Ejem1_ComponentesBasicos/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.ejemp01_componentesbasicos"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.ejemp01_componentesbasicos"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
     }
 }
 
