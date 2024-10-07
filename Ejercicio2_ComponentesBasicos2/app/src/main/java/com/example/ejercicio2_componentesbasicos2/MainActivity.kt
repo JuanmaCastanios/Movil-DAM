d3896416f1b4d348558fa293480fdffe7c21b53f
JuanmaCastanios <juanjoseblancodiaz1504@gmail.com>
2024-10-07 11:26:59 +0200

    Ejemplos

--- Ejercicio2_ComponentesBasicos2/app/src/main/java/com/example/ejercicio2_componentesbasicos2/MainActivity.kt
+++ Ejercicio2_ComponentesBasicos2/app/src/main/java/com/example/ejercicio2_componentesbasicos2/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.ejercicio2_componentesbasicos2
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
+import android.text.method.KeyListener
 import android.util.Log
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.RadioButton
 import androidx.activity.enableEdgeToEdge
@@ -22,6 +24,23 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.textoDatos.setOnKeyListener { componentePAdre, codigoLetra, evento ->
+
+            Log.d("depurando", "Ha pulsado " + codigoLetra)
+            true
+        }
+
+        binding.padreRojo.setOnTouchListener { view, motionEvent ->
+            Log.d("depurando", "Ha pulsado en el padre")
+            true
+        }
+
+        binding.hijoAmarillo.setOnTouchListener { view, motionEvent ->
+            Log.d("depurando", "Ha pulsado en el hijo")
+            binding.textoHijoAmarillo.text=motionEvent.x.toString()+","+motionEvent.y.toString()
+            true
+        }
+
         /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -31,6 +50,9 @@ class MainActivity : AppCompatActivity() {
          */
         var datos_Desplegable = arrayOf("Valladolid", "Vigo", "Madrid", "Córdoba")
         binding.selectorCiudades.adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, datos_Desplegable)
+
+        var persona: Persona = Persona("Juan", 20, true, listOf("Futbol", "Baloncesto"))
+        binding.usuario = persona
 
         binding.botonGuardar.setOnClickListener(){
             var nombre: String = binding.textoDatos.text.toString()
@@ -45,8 +67,6 @@ class MainActivity : AppCompatActivity() {
             if(binding.checkBaloncesto.isChecked) deportes.add("Balonceste")
             if(binding.checkTenis.isChecked) deportes.add("Tenis")
 
-            var persona: Persona = Persona(binding.usuario.nombre)
-
 
             var personaVacia: Persona =  Persona()
             binding.usuario = personaVacia
@@ -55,9 +75,6 @@ class MainActivity : AppCompatActivity() {
             Log.d("Datos Formulario", "Estado civil:  ${estadoCivil}")
             Log.d("Datos Formulario", "Ciudad: ${ciudad}")
             Log.d("Datos Formulario", "Deportes: ${deportes.joinToString(", ")}")
-            if (persona != null) {
-                Log.d("Datos Guardados", persona.nombre)
-            }
         }
 
 
--- Ejercicio2_ComponentesBasicos2/app/src/main/java/com/example/ejercicio2_componentesbasicos2/Persona.kt
+++ Ejercicio2_ComponentesBasicos2/app/src/main/java/com/example/ejercicio2_componentesbasicos2/Persona.kt
@@ -2,10 +2,16 @@ package com.example.ejercicio2_componentesbasicos2
 
 class Persona {
     var nombre:String = ""
+    var soltero:Boolean = true
+    var edad: Int = 0
+    var deportes: List<String> = emptyList()
     constructor(){
         this.nombre = ""
     }
-    constructor( nombre:String){
+    constructor( nombre:String, edad:Int, soltero: Boolean, deportes:List<String>){
         this.nombre = nombre
+        this.soltero = soltero
+        this.edad = edad
+        this.deportes = deportes
     }
 }
