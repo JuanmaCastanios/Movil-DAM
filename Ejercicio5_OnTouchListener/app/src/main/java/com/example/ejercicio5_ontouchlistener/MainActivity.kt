1c01a4d59fdbfd726c061eb4da178ee65ee6c3f0
JuanmaCastanios <juanjoseblancodiaz1504@gmail.com>
2024-10-18 09:59:31 +0200

    ejemplos constraint layout

--- Ejercicio5_OnTouchListener/app/src/main/java/com/example/ejercicio5_ontouchlistener/MainActivity.kt
+++ Ejercicio5_OnTouchListener/app/src/main/java/com/example/ejercicio5_ontouchlistener/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.ejercicio5_ontouchlistener
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.MotionEvent
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -21,32 +22,7 @@ class MainActivity : AppCompatActivity() {
 
         var usuarios : Array<Persona> = arrayOf(Persona("Juan", "1111"), Persona("José", "2222"), Persona("Pepe", "3333"))
         var adaptador = ArrayAdapter(this, android.R.layout.simple_list_item_1,usuarios)
-        var movimientoRegistrado: Boolean = false
 
         binding.listaUsuarios.adapter = adaptador
-
-        binding.listaUsuarios.setOnItemClickListener { adapterView, view, i, l ->
-            for ( i in usuarios){
-                if(i.nombre == binding.listaUsuarios.selectedItem.toString())
-                    binding.usuario.nombre = i.nombre
-                    binding.usuario.contrasena = i.contrasena
-            }
-        }
-
-        binding.recuadroAzul.setOnTouchListener { _, event ->
-            when (event.action) {
-                MotionEvent.ACTION_MOVE -> {
-                    if (!movimientoRegistrado) {
-                        movimientoRegistrado = true
-
-                    }
-                }
-                MotionEvent.ACTION_UP -> {
-                    movimientoRegistrado = false
-                }
-            }
-            false
-        }
-
     }
 }
